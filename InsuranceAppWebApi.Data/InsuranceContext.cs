fb7c3896adfbf06d3a68dc1a7f7b58d263d668fb
Rafał Kelm <rkelm@efigence.com>
2021-08-28 19:44:05 +0200

    connecting db context

--- InsuranceAppWebApi.Data/InsuranceContext.cs
+++ InsuranceAppWebApi.Data/InsuranceContext.cs
@@ -1,23 +1,17 @@
 ﻿using InsuranceAppWebApi.Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
 
 namespace InsuranceAppWebApi.Data
 {
     public class InsuranceContext: DbContext
     {
+        public InsuranceContext(DbContextOptions<InsuranceContext> options):base(options)
+        {
+
+        }
         public DbSet<Insurance> Insurances { get; set; }
         public DbSet<InsuranceCompany> InsuranceCompanies { get; set; }
         public DbSet<Insuree> Insurees { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //logging enabled
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=InsuranceAppWebApiData")
-                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
-                .EnableSensitiveDataLogging();
-        }
 
         protected override void OnModelCreating(ModelBuilder modelbuilder)
         {
--- InsuranceAppWebApiAPI/Startup.cs
+++ InsuranceAppWebApiAPI/Startup.cs
@@ -1,6 +1,8 @@
+using InsuranceAppWebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,9 @@ namespace InsuranceAppWebApiAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<InsuranceContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("InsuranceWebApiConnection"))
+            .EnableSensitiveDataLogging()
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
